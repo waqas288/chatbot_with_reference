d36aa383423fb7105a3f0f03017a58480d4b41a4
waqas288 <mohammadwaqas0288@gmail.com>
2025-11-26 00:10:55 +0500

    n

--- create_memory_for_llm.py
+++ create_memory_for_llm.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
+from langchain_chroma import Chroma
 
 ## Uncomment the following files if you're not using pipenv as your virtual environment manager
 from dotenv import load_dotenv, find_dotenv
@@ -19,7 +19,7 @@ def load_pdf_files(data):
     return documents
 
 documents=load_pdf_files(data=DATA_PATH)
-#print("Length of PDF pages: ", len(documents))
+print("Length of PDF pages: ", len(documents))
 
 
 # Step 2: Create Chunks
@@ -30,7 +30,7 @@ def create_chunks(extracted_data):
     return text_chunks
 
 text_chunks=create_chunks(extracted_data=documents)
-#print("Length of Text Chunks: ", len(text_chunks))
+print("Length of Text Chunks: ", len(text_chunks))
 
 # Step 3: Create Vector Embeddings 
 
@@ -40,7 +40,7 @@ def get_embedding_model():
 
 embedding_model=get_embedding_model()
 
-# Step 4: Store embeddings in FAISS
-DB_FAISS_PATH="vectorstore/db_faiss"
-db=FAISS.from_documents(text_chunks, embedding_model)
-db.save_local(DB_FAISS_PATH)
+# Step 4: Store embeddings in ChromaDB
+DB_CHROMA_PATH="vectorstore/db_chroma"
+db=Chroma.from_documents(text_chunks, embedding_model, persist_directory=DB_CHROMA_PATH)
+print(f"ChromaDB created successfully at {DB_CHROMA_PATH}")
